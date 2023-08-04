1e31d362b0e7ab3504a2411292f13a466c73ca8a
Justin Ang <jrang@sfu.ca>
2023-08-04 15:42:50 -0700

    update to my stuff

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'sanity';
-import { deskTool } from 'sanity/desk';
-import schemas from "./sanity/schemas"
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import schemas from "./sanity/schemas";
 
 const config = defineConfig({
-  projectId: "txd29ab2",
+  projectId: "4fe2wuae",
   dataset: "production",
-  title: "My Personal Website",
+  title: "sanity-test",
   apiVersion: "2023-03-09",
   basePath: "/admin",
-  plugins: [deskTool()],
-  schema: { types: schemas }
-})
+  plugins: [deskTool(), visionTool()],
+  schema: { types: schemas },
+});
 
-export default config
+export default config;
